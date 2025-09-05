9b01ff7c0d4e37e2735d81ca613d0ea9aed5d27e
Nupur Chendge <nupur.chendge@gmail.com>
2025-09-05 20:02:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (31.99%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+        int k=0;
+         while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+        while(i>=0){
+            if(s[i]!=' '){
+                k++;
+            }
+            else{
+                break;
+            }
+            i--;
+        }
+        return k;
+    }
+};
