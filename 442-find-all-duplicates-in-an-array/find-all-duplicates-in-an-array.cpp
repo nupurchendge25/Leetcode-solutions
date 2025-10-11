fba424f88a4d68e4054069b1fa1280e8dd60f8a7
Nupur Chendge <nupur.chendge@gmail.com>
2025-10-11 19:50:14 +0530

    Time: 23 ms (36.08%) | Memory: 48.7 MB (43.51%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int> ans;
+        int i=0;
+        int n=nums.size();
+        while(i<n-1){
+            if(nums[i]==nums[i+1]){
+                ans.push_back(nums[i]);
+                i=i+2;
+            }
+            else{
+                i++;
+            }
+        }
+        return ans;
+    }
+};
