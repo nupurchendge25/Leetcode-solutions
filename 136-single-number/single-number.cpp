5906bb52778d4d6aa54e83364a74019a8426cc90
Nupur Chendge <nupur.chendge@gmail.com>
2026-01-13 06:54:39 -0800

    Time: 0 ms (100.00%) | Memory: 20.6 MB (82.38%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res = 0;
-    for(int num : nums) {
-        res ^= num;
-    }
-    return res;
+        int size=nums.size();
+        int x=0;
+        for(int i=0;i<size;i++){
+            x=x^nums[i];
+        }
+        return x;
     }
 };
