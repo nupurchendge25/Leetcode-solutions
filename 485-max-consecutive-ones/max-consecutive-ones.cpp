8baf677791b0b5210376477ef267e52e451be259
Nupur Chendge <nupur.chendge@gmail.com>
2026-01-17 04:48:50 -0800

    Time: 0 ms (100.00%) | Memory: 50.2 MB (32.62%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n=nums.size();
+
+        int count=0;
+        int max=0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                count++;
+            }
+            else{
+                count=0;
+            }
+
+            if(count>max){
+                max=count;
+            }
+        }
+        return max;
+    }
+};
