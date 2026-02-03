befe73ae839a57e4dfb8a174e5e4a8d53df74fbc
Nupur Chendge <nupur.chendge@gmail.com>
2026-02-02 22:29:55 -0800

    Time: 2 ms (39.06%) | Memory: 14.1 MB (10.82%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+
+        for(auto it : freq){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
