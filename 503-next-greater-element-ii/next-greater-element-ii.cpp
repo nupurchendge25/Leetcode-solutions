d2b4eb077448a480a7c4c0fb0c9dcadc136b25bc
Nupur Chendge <nupur.chendge@gmail.com>
2026-02-02 17:24:31 -0800

    Time: 0 ms (100.00%) | Memory: 28.4 MB (59.24%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        int n=nums.size();
+        vector<int> result(n,-1);
+
+        for(int i=0;i<2*n;i++){
+            int idx=i%n;
+            while(!st.empty() && nums[st.top()]<nums[idx]){
+                result[st.top()]=nums[idx];
+                st.pop();
+            }
+            if(i<n){
+                st.push(idx);
+            }
+        }
+        return result;
+    }
+};
