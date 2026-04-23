f1704f93dc685ff11a502082074bc0be2841c95c
Nupur Chendge <nupur.chendge@gmail.com>
2026-04-22 22:02:31 -0700

    Time: 18 ms (29.44%) | Memory: 18.9 MB (36.04%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> result;
+    
+        for( string p: queries){
+            for(string d : dictionary){
+                int diff=0;
+                for(int i=0;i<p.size();i++){
+                    if(p[i]!=d[i]) diff++;
+                }
+                if(diff<=2){
+                     result.push_back(p) ;
+                     break;
+                }
+            }
+        }
+        return result;
+    }
+};
