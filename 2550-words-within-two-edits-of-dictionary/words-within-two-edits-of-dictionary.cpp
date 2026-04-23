08a881af33416a1897912e78e8ae0ddfb30718e6
Nupur Chendge <nupur.chendge@gmail.com>
2026-04-22 22:05:57 -0700

    Time: 0 ms (100.00%) | Memory: 12.6 MB (71.57%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -8,6 +8,7 @@ public:
                 int diff=0;
                 for(int i=0;i<p.size();i++){
                     if(p[i]!=d[i]) diff++;
+                    if(diff>2) break;
                 }
                 if(diff<=2){
                      result.push_back(p) ;
